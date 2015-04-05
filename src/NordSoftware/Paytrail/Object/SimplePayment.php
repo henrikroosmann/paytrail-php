00aee9e843ab1141759958f96c823b0742195ecd
Henri Kroosmann <henri@kroosmann.ee>
2015-04-06 00:29:17 +0300

    Updates to become an extension

--- src/NordSoftware/Paytrail/Object/SimplePayment.php
+++ src/NordSoftware/Paytrail/Object/SimplePayment.php
@@ -10,77 +10,12 @@
 
 namespace NordSoftware\Paytrail\Object;
 
-use NordSoftware\Paytrail\Common\DataObject;
-use NordSoftware\Paytrail\Exception\TooManyProducts;
-use NordSoftware\Paytrail\Exception\CurrencyNotSupported;
-use NordSoftware\Paytrail\Exception\LocaleNotSupported;
-
-class SimplePayment extends DataObject
+class SimplePayment extends Payment
 {
-    const CURRENCY_EUR = 'EUR';
-
-    const LOCALE_FIFI  = 'fi_FI';
-    const LOCALE_SVSE  = 'sv_SE';
-    const LOCALE_ENUS  = 'en_US';
-
-    const VAT_MODE_INCLUDED = 1;
-    const VAT_MODE_EXCLUDED = 0;
-
-    /**
-     * @var string
-     */
-    protected $orderNumber;
-
-    /**
-     * @var string
-     */
-    protected $referenceNumber = '';
-
-    /**
-     * @var string
-     */
-    protected $description = '';
-
-    /**
-     * @var string
-     */
-    protected $currency = self::CURRENCY_EUR;
-
-    /**
-     * @var string
-     */
-    protected $locale = self::LOCALE_FIFI;
-
-    /**
-     * @var int
-     */
-    protected $vatMode = self::VAT_MODE_INCLUDED;
-
     /**
      * @var float
      */
     protected $price;
-
-    /**
-     * @var \NordSoftware\Paytrail\Object\UrlSet
-     */
-    protected $urlSet;
-
-    /**
-     * @var array
-     */
-    static $supportedCurrencies = array(
-        self::CURRENCY_EUR,
-    );
-
-    /**
-     * @var array
-     */
-    static $supportedLocales = array(
-        self::LOCALE_FIFI,
-        self::LOCALE_SVSE,
-        self::LOCALE_ENUS,
-    );
 
     /**
      * @return array
@@ -108,86 +43,6 @@ class SimplePayment extends DataObject
             $array['urlSet'] = $this->urlSet->toArray();
         }
         return $array;
-    }
-
-    /**
-     * @return string
-     */
-    public function getOrderNumber()
-    {
-        return $this->orderNumber;
-    }
-
-    /**
-     * @return string
-     */
-    public function getReferenceNumber()
-    {
-        return $this->referenceNumber;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    /**
-     * @param string $currency
-     * @throws \NordSoftware\Paytrail\Exception\CurrencyNotSupported
-     */
-    public function setCurrency($currency)
-    {
-        if (!in_array($currency, self::$supportedCurrencies)) {
-            throw new CurrencyNotSupported(sprintf('Currency "%s" is not supported.', $currency));
-        }
-        $this->currency = $currency;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCurrency()
-    {
-        return $this->currency;
-    }
-
-    /**
-     * @param string $locale
-     * @throws @throws \NordSoftware\Paytrail\Exception\LocaleNotSupported
-     */
-    public function setLocale($locale)
-    {
-        if (!in_array($locale, self::$supportedLocales)) {
-            throw new LocaleNotSupported(sprintf('Locale "%s" is not supported.', $locale));
-        }
-        $this->locale = $locale;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLocale()
-    {
-        return $this->locale;
-    }
-
-    /**
-     * @return int
-     */
-    public function getVatMode()
-    {
-        return $this->vatMode;
-    }
-
-    /**
-     * @return \NordSoftware\Paytrail\Object\UrlSet
-     */
-    public function getUrlSet()
-    {
-        return $this->urlSet;
     }
 
     /**
